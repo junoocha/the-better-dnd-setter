eb059d3fde3f6115073be7542b76d57553b2ca3a
Caleb K Cha <junoocha@gmail.com>
2024-12-27 22:01:08 -0500

    now i properly added tailwindcss

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p class=" py-12 px-16">
+          Edit src/App.js and save to reload.
         </p>
         <a
-          className="App-link"
+          class="App-link text-blue-500 hover:text-blue-700"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
