5797dde87094ea6b812db46d530901e67e2a5b3e
[Caleb Cha] <junoocha@gmail.com>
2025-05-05 15:50:45 -0400

    some more sentences + new idea on separate arrays

--- src/components/text-components/intro-text.tsx
+++ src/components/text-components/intro-text.tsx
@@ -5,8 +5,8 @@ type Props = {
 };
 
 export default function IntroText({ onComplete }: Props) {
+	const intro = ["Welcome to the D&D Character Stat Setter."];
 	const sentences = [
-		"Welcome to the D&D Character Stat Setter.",
 		"Hello? Are you still there?",
 		"You tryna make a character or what?",
 		"Can you wake him up? I've got stuff to do.",
@@ -14,10 +14,27 @@ export default function IntroText({ onComplete }: Props) {
 		"I hate my job.",
 		"See that blue rectangle? Wanna left click on that?",
 		"Please wake up the bard so I can leave.",
+		"I'm literally just text, and I'm already stressed out.",
+		"Did you fall asleep? Should I come back later?",
+		"I'm not saying you're slow, but the tortoise is worried about you.",
+		"Stat-setting shouldn't be this emotionally taxing.",
+		"If I had a copper for every second you're wasting...",
+		"The bard is dreaming about spreadsheets. Wake him up.",
+		"This is why wizards don't let the party handle UI.",
+		"You know this isn't turn-based, right?",
+		"The blue button isn’t gonna press itself, champ.",
+		"At this rate, the villain's already won. AKA the dungeon master.",
+		"Press the button before I roll a nat 1 on patience.",
+		"You're really making me miss the goblin encounter.",
+		"I'm just the intro text, and even I want a break.",
+		"If passive-aggressive tooltips were a spell, you'd be toast.",
+		"Any slower and this would be a loading screen in Skyrim.",
+		"Even children on Roblox can press a button bro.",
 	];
 
 	return (
 		<div className="flex flex-col gap-6 items-center text-center">
+			<TextAnimation sentences={intro} fadeTrue={false} />
 			<TextAnimation sentences={sentences} fadeTrue={false} />
 			{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
 			<button
