a54462462ea31c6ccf9de25f239fa973a5397289
[Caleb Cha] <junoocha@gmail.com>
2025-05-05 16:17:10 -0400

    now can have 2 different arrays

--- src/components/TextAnimation.tsx
+++ src/components/TextAnimation.tsx
@@ -2,15 +2,17 @@ import { useState, useEffect } from "react";
 
 // props for tsx
 type TextAnimationProps = {
+	initialSentences: string[];
+	loopSentences: string[];
 	fadeTrue: boolean;
-	sentences: string[];
 	speed?: number;
 	delayBetweenSentences?: number;
 	fadeDuration?: number;
 };
 
 const TextAnimation = ({
-	sentences, // array of sentences to be inputted
+	initialSentences, // array of sentences to be inputted
+	loopSentences,
 	fadeTrue, // if true, should fade automatically, if not, will fade when step ends
 	speed = 50,
 	delayBetweenSentences = 1000,
@@ -19,11 +21,22 @@ const TextAnimation = ({
 	const [displayedText, setDisplayedText] = useState<string>(""); // useState for the displayed text
 	const [currentSentenceIndex, setCurrentSentenceIndex] = useState<number>(0); // use state for the current index of the sentence (if multiple sentences used)
 	const [fadingOut, setFadingOut] = useState<boolean>(false); // fade out boolean
+	const [isInLoopPhase, setIsInLoopPhase] = useState<boolean>(false);
+
+	// flip flop between sentence array
+	const sentences = isInLoopPhase ? loopSentences : initialSentences;
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
 		// if the index reaches end of array, just end/return
-		if (currentSentenceIndex >= sentences.length) return;
+		if (currentSentenceIndex >= sentences.length) {
+			// transition to loop sentences after initial ones
+			if (!isInLoopPhase && loopSentences.length > 0) {
+				setCurrentSentenceIndex(0);
+				setIsInLoopPhase(true);
+			}
+			return;
+		}
 
 		// get the first sentence
 		const currentSentence = sentences[currentSentenceIndex];
@@ -81,6 +94,8 @@ const TextAnimation = ({
 		speed,
 		delayBetweenSentences,
 		fadeDuration,
+		isInLoopPhase,
+		loopSentences,
 	]);
 
 	return (
