9cb442dffb5854ff9b90ad8879382a563356d000
[Caleb Cha] <junoocha@gmail.com>
2025-06-10 18:47:35 -0400

    removed unnecessary supabase_private, just in case

--- utils/supabase_private.ts
+++
@@ -1,9 +0,0 @@
-// for server side api
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	process.env.NEXT_PUBLIC_SUPABASE_URL!,
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	process.env.SUPABASE_SERVICE_ROLE_KEY!,
-);
